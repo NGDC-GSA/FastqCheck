bde64f4db4e524d64e4f915b7fc5b4e54735ce72
XLZH <xiaolongzhang2015@163.com>
2026-08-14 17:35:38 +0800

    feat: add the required parameters of max_reads and phred_value
    - add the -x|--max_reads and -P|--phred_value required parameters (estimated by fastq_type) and validate the phred value (33 or 64)
    - rename the ratio member to compress_ratio and use int32_t/uint64_t types
    - bump the version to 2.0.0 (update date: 2026-07-30)

--- params_parse.h
+++ params_parse.h
@@ -12,11 +12,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <getopt.h>
 
-#define __FASTQ_CHECK_VERSION__ "1.1.21"
+#define __FASTQ_CHECK_VERSION__ "2.0.0"
 #define __FASTQ_CHECK_CREATE_DATE__ "2022-07-25"
-#define __FASTQ_CHECK_UPDATE_DATE__ "2024-04-26"
+#define __FASTQ_CHECK_UPDATE_DATE__ "2026-07-30"
 
 
 /*! @typedef arg_t
@@ -25,17 +26,21 @@
  @field max_length      the maximum length allowed for one read sequence [default:50MB]
  @field pair_check      check whether all reads have the same pair marker (1->check; 0->ignore) [default:1]
  @field n_thread        number of thread used in fastq processing [default:1]
- @field ratio           the compression ratio of the fastq file [default:10]
+ @field compress_ratio  the compression ratio of the fastq file [default:10]
+ @field max_reads       the estimated maximum number of reads (estimated by fastq_type, in million) [required]
+ @field phred_value     the phred value of the fastq file (estimated by fastq_type, 33 or 64) [required]
  @field in_file         fastq file list (eg. samples.txt)
  @field out_file        output file of the given fastq files after checking (eg. output.xml)
  @field error_file      warning or error message about the given fastq files
 */
 typedef struct arg_t {
-    int help;
-    int max_length;
-    int pair_check;
-    int n_thread;
-    int ratio;
+    int32_t help;
+    int32_t max_length;
+    int32_t pair_check;
+    int32_t n_thread;
+    int32_t compress_ratio;
+    uint64_t max_reads;
+    int32_t phred_value;
     char *in_file;
     char *out_file;
     char *error_file;
